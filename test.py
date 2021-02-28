1dd9344eab5bf7f40bd29b5c388fda04b6a07133
s-ovsiuk <s.ovsiuk@gmail.com>
2021-02-28 21:13:29 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,26 @@
+# task number five
+
+number1 = 20
+number2 = 30
+
+print(number1 + number2)
+print(number1 * number2)
+print(number1 - number2)
+print(number1 / number2)
+
+# task number six
+
+my_list = list(range(1, 11))
+new_list = my_list + [20, 21, 22, 23, 24, 25]
+
+new_list[0]
+new_list[-1]
+
+new_list[:4]
+new_list[::-1]
+new_list[1:]
+
+
+
+
+
